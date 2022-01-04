bf1331fc31d71393b22011842140b81d06d96804
Em <emilylauder@gmail.com>
2022-01-04 01:32:43 -0500

    adding dummy data for later

---
+++ src/data.js
@@ -0,0 +1,98 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 2,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 3,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 4,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 5,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 6,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+];
+
+export const webApps = [
+  {
+    id: 1,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 2,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 3,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 4,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 5,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 6,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 2,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 3,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 4,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 5,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+  {
+    id: 6,
+    Title: "Enter Title for project",
+    img: "url",
+  },
+];
