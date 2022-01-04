0e011ac7852134f522854371e10ed6dadb10f9e1
Em <emilylauder@gmail.com>
2022-01-04 00:12:55 -0500

    adding assets

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -5,10 +5,21 @@ export default function intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/dance.png" alt="" />
+          <img src="assets/man.png" alt="person" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello! I'm</h2>
+          <h1>Emily Lane</h1>
+          <h3>
+            Front End <span>Developer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down arrow" />
+        </a>
+      </div>
     </div>
   );
 }
