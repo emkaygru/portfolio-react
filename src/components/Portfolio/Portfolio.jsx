683ea2bd032ec48bd59afafab02f092a1c0bb053
Em <emilylauder@gmail.com>
2022-01-04 01:27:46 -0500

    styling portfolio section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,71 @@
+import { useEffect, useState } from "react";
 import "./Portfolio.scss";
+import PortfolioList from "../PortfolioList/PortfolioList";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "branding",
+      title: "Branding",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-      portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+
+      <div className="container">
+        <div className="item">
+          <img src="./assets/app.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/app.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/app.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/app.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/app.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/app.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
