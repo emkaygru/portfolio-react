4876284e34c6c172ca6512edcb4a01deb971a7ad
Em <emilylauder@gmail.com>
2022-01-03 22:52:58 -0500

    updating TopBar styling - adding icons

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,4 +1,6 @@
 import "./TopBar.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import MailIcon from "@mui/icons-material/Mail";
 
 export default function TopBar() {
   return (
@@ -8,9 +10,19 @@ export default function TopBar() {
           <a href="#intro" className="logo">
             emily lane
           </a>
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>+1-720-323-8220</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>lane.emilykay@gmail.com</span>
+          </div>
         </div>
-        <div className="right">right</div>
+      </div>
+
+      <div className="right">
+        <MailIcon className="icon" />
       </div>
     </div>
   );
